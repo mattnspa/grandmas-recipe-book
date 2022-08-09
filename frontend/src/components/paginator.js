efcdaa70b725a6ac25405f563dfbfeb4bd44f815
Matteo m1 Mac <mnspanu@hotmail.com>
2022-08-09 23:07:38 +0100

    change pagination logic

--- frontend/src/components/paginator.js
+++ frontend/src/components/paginator.js
@@ -1,30 +1,35 @@
 import Pagination from 'react-bootstrap/Pagination';
 
 export const Paginator = props => {
-  // console.log(handleClick)
-  console.log(props)
   const { currentPage, previousPage, nextPage, lastPage, handleClick} = props;
+
+  const pagePriority = [
+    currentPage,
+    previousPage,
+    nextPage,
+    previousPage - 1,
+    nextPage + 1,
+    previousPage - 2,
+    nextPage + 2,
+    previousPage - 3,
+    nextPage + 3,
+  ];
+
+  const arr = pagePriority.filter(page => {return (!isNaN(page) && page > 0 && page <= lastPage);})
+  const uniquePageArr = [...new Set(arr)];
+  const pageArr = uniquePageArr.slice(0,5)
+  pageArr.sort();
+
   return (
     <Pagination>
-      {/* If previous page defined, show previous page button*/}
-      {previousPage && <Pagination.Prev onClick={() => {handleClick(previousPage)}}/>}
-      {/* If current page not first page, show first page button*/}
-      {(currentPage !== 1) && <Pagination.Item onClick={() => {handleClick(1)}} >{1}</Pagination.Item>}
-      {/* If previous page and previous previous page not first page and currently not on first page, show ellipsis*/}
-      {(previousPage !== 1) && (previousPage - 1 !== 1) && (currentPage !== 1) && <Pagination.Ellipsis onClick={() => {handleClick(previousPage - 1)}} />}
-      {/* If previous page not first page and previous page is defined, show current page - 1 */}
-      {(previousPage !== 1) && previousPage && <Pagination.Item onClick={() => {handleClick(previousPage)}} >{previousPage}</Pagination.Item>}
+      <Pagination.Prev disabled={!previousPage} onClick={() => {handleClick(previousPage)}}/>
       
-      <Pagination.Item active>{currentPage}</Pagination.Item>
-      
-      {/* If next page not last page and next page is defined, show current page + 1 */}
-      {(nextPage !== lastPage) && nextPage && <Pagination.Item onClick={() => {handleClick(nextPage)}} >{nextPage}</Pagination.Item>}
-      {/* If next page and next next page not last page and currently not on last page, show ellipsis*/}
-      {(nextPage !== lastPage) && (nextPage + 1 !== lastPage) &&  (currentPage !== lastPage) && <Pagination.Ellipsis onClick={() => {handleClick(nextPage + 1)}} />}
-      {/* If current page not last page, show last page button*/}
-      {(currentPage !== lastPage) && <Pagination.Item onClick={() => {handleClick(lastPage)}}>{lastPage}</Pagination.Item>}
-      {/* If next page defined, show next page button*/}
-      {nextPage && <Pagination.Next onClick={() => {handleClick(nextPage)}} />}
+      {pageArr.map((page,index) => <Pagination.Item key={page} active={page === currentPage ? true : false}  onClick={() => {handleClick(page)}}>
+        {page}
+        </Pagination.Item>
+        )}
+
+      <Pagination.Next  disabled={!nextPage} onClick={() => {handleClick(nextPage)}} />
     </Pagination>
   );
 }
--- frontend/src/pages/allRecipes.js
+++ frontend/src/pages/allRecipes.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
-import Button from "react-bootstrap/esm/Button";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row";
 
 import { RecipeCard } from "../components/recipeCards";
@@ -32,19 +30,17 @@ export const AllRecipesPage = () => {
   if (isLoading) return <div className="App">Loading...</div>;
 
   return (
-    <div>
-      <Paginator handleClick={pageChange} {...recipes.paging} />      
-      <Container fluid >
-        <Card className="bg-light m-5">
-          <Row >
-            { recipes.data?.map(recipe => (
-              <Col align="center" className="mt-5" key={recipe.id}>
-                <RecipeCard recipe={recipe} />
-            </Col>
-            ))}
-          </Row>
-        </Card>
-      </Container>
+    <div>   
+      <Card className="bg-light m-5">
+        <Paginator handleClick={pageChange} {...recipes.paging} />   
+        <Row >
+          { recipes.data?.map(recipe => (
+            <Col align="center" className="mt-5" key={recipe.id}>
+              <RecipeCard recipe={recipe} />
+          </Col>
+          ))}
+        </Row>
+      </Card>
     </div>
   );
 }
