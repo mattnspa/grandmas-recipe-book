96f65757cce4629819a78ded9de3c3ca1ab039b4
Matteo m1 Mac <mnspanu@hotmail.com>
2022-10-25 07:15:48 +0100

    Add suggestions to searchbar results

---
+++ frontend/src/components/search/searchbar.js
@@ -0,0 +1,27 @@
+import { useRef, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/esm/InputGroup';
+import Form from 'react-bootstrap/Form';
+import { Results } from './results';
+
+export const SearchBar = () => {
+  const [query,setQuery] = useState("");
+  const target = useRef(null);
+
+  return (
+    <Form >
+      <InputGroup>
+        <Form.Control
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          name="q"
+          onChange={e => setQuery(e.target.value)}
+          ref={target}
+        />
+        <Button type="submit" variant="outline-success" >Search</Button>
+      </InputGroup>
+      <Results query={query} target={target}/>
+    </Form>
+  );
+}
